32dfabb6dd56b2873d7d5733876cd6179e92ffb6
Sergey Antonov <asergey96@gmail.com>
2017-05-25 02:43:17 +0300

    added user profile, updated front-end

---
+++ task2/app/controllers/profiles_controller.rb
@@ -0,0 +1,8 @@
+class ProfilesController < ApplicationController
+  expose_decorated :user
+
+  def show
+    user = User.find(params[:id])
+  end
+
+end
--- task2/config/routes.rb
+++ task2/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :events
+  resources :prifles
+
   devise_for :users, :controllers => { registrations: 'users/registrations' }
+
+  get 'profiles/:id', to: 'profiles#show'
 
   root to: "events#index"
 end
